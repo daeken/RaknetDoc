a056a4f64ced0eca1436ffb95b1db3d010569a1e
Serafina Brocious <sera.brocious@gmail.com>
2024-12-10 14:54:01 -0500

    Edited template a bit. Added CC-BY license.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'RaknetDoc'
+copyright = '2024, Sera Brocious'
+author = 'Sera Brocious'
 
 release = '0.1'
 version = '0.1.0'
